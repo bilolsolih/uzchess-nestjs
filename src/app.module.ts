2a56ef33ef82ff2d0fc267f9f8107d4fb2ee089d
SolihCoder <BilolMuhammadSolih@gmail.com>
2026-02-28 11:40:10 +0500

    Initial Commit

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,17 @@
-import {Module} from '@nestjs/common';
+import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { AuthenticationModule } from './features/authentication/authentication.module';
 
 @Module({
-    imports: [],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      url: process.env.DB_URL,
+      synchronize: true,
+      autoLoadEntities: true,
+      entities: ['./dist/**/*.entity.js'],
+    }),
+    AuthenticationModule,
+  ],
 })
-export class AppModule {
-}
+export class AppModule {}
