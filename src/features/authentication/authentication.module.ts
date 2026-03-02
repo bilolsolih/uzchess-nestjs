5ab71cafa6cd1c17e2820131a9cb5b600111bf6a
SolihCoder <BilolMuhammadSolih@gmail.com>
2026-03-02 14:00:49 +0500

    outsourced the JWT Module configuration in the authentication.module.ts

--- src/features/authentication/authentication.module.ts
+++ src/features/authentication/authentication.module.ts
@@ -2,17 +2,10 @@ import { Module } from '@nestjs/common';
 import { JwtModule } from '@nestjs/jwt';
 import { AuthenticationService } from './services/authentication.service';
 import { OtpCodeService } from './services/otp-code.service';
+import { jwtModuleConfig } from '../../configs/jwt-module.config';
 
 @Module({
-  imports: [
-    JwtModule.register({
-      global: true,
-      secret: process.env.SECRET_KEY,
-      signOptions: {
-        expiresIn: '3h',
-      },
-    }),
-  ],
+  imports: [JwtModule.register(jwtModuleConfig)],
   providers: [AuthenticationService, OtpCodeService],
 })
 export class AuthenticationModule {}
