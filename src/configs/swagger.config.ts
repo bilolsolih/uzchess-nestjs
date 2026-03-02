1aef490545156c35854ed6f4355af24d48a96e88
SolihCoder <BilolMuhammadSolih@gmail.com>
2026-03-02 13:51:36 +0500

    outsourced swagger and type-orm configurations to the configs folder

---
+++ src/configs/swagger.config.ts
@@ -0,0 +1,12 @@
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import { INestApplication } from '@nestjs/common';
+
+export const configureSwagger = (app: INestApplication) => {
+  let swaggerConfig = new DocumentBuilder()
+    .setTitle('UzChess')
+    .setVersion('1.0.0')
+    .build();
+
+  let docs = SwaggerModule.createDocument(app, swaggerConfig);
+  SwaggerModule.setup('/docs', app, docs);
+};
