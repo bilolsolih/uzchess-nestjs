3b50f8e64a515fe4d3b9e0fbe9f6511530f316c8
SolihCoder <BilolMuhammadSolih@gmail.com>
2026-03-02 13:52:36 +0500

    created Role enum, Roles decorator and RolesGuard

---
+++ src/core/guards/roles.guard.ts
@@ -0,0 +1,25 @@
+import { CanActivate, ExecutionContext, Injectable, UnauthorizedException } from '@nestjs/common';
+import { Observable } from 'rxjs';
+import { Reflector } from '@nestjs/core';
+import { RolesKey } from '../decorators/roles.decorator';
+import { Role } from '../enums/role.enum';
+
+@Injectable()
+export class RolesGuard implements CanActivate {
+  constructor(private readonly reflector: Reflector) {}
+
+  canActivate(context: ExecutionContext): boolean | Promise<boolean> | Observable<boolean> {
+    let roles = this.reflector.getAllAndOverride<Role[]>(RolesKey, [context.getHandler(), context.getClass()]);
+    if (!roles) {
+      return true;
+    }
+
+    let { user } = context.switchToHttp().getRequest();
+
+    if (!user) {
+      throw new UnauthorizedException('Credentials were not found');
+    }
+
+    return roles.includes(user.role);
+  }
+}
