c63494f407555f6271098e0abd06f88af3b69d92
SolihCoder <BilolMuhammadSolih@gmail.com>
2026-03-02 14:17:25 +0500

    refactored the configurations

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,20 @@
 import 'dotenv/config';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ValidationPipe } from '@nestjs/common';
+import { configureSwagger } from './configs/swagger.config';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  configureSwagger(app);
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true,
+      forbidNonWhitelisted: true,
+      transform: true,
+    }),
+  );
+
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
